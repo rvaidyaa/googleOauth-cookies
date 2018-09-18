3f52537751e1a5c5c79bcc8cc45aa5f5a55b3412
gregsprog <gregsprog@gmail.com>
2018-09-18 11:40:54 -0700

    troubleshoot

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,8 @@
 module.exports = {
     googleClientID:process.env.GOOGLE_CLIENT_ID,
     googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    mongoURI:'mongodb:admin:Baloon00@ds161062.mlab.com:61062/email-rahul-prod',
-    cookieKey:COOKIE_KEY
+    mongoURI:process.env.MONGO_URI,
+    cookieKey:process.env.COOKIE_KEY
 };
 
 
