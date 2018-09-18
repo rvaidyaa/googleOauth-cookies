b0fa786b0bb04c0104a806add44d08234ce71c7e
gregsprog <gregsprog@gmail.com>
2018-09-18 11:22:40 -0700

    removed quotes

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 // production keys here
 // get variables f
 module.exports = {
-    googleClientID:'process.env.GOOGLE_CLIENT_ID',
-    googleClientSecret:'process.env.GOOGLE_CLIENT_SECRET',
-    mongoURI:'MONGO_URI',
-    cookieKey:'COOKIE_KEY'
+    googleClientID:process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI:MONGO_URI,
+    cookieKey:COOKIE_KEY
 };
