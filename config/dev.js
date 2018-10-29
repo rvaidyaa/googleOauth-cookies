8d9c3ab0869b76e051ca95918817201910813fe3
rvaidyaa <rvaidyaa@gmail.com>
2018-10-29 10:10:26 -0700

    testing

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    googleClientID:'500242202169-vlamg6klrpegk1pn01talirfdohnavj7.apps.googleusercontent.com',
+    googleClientSecret:'6AqXLyQfSTG35sjJr3Xb0OdH',
+    mongoURI:'mongodb://rahul:Baloon00@ds137207.mlab.com:37207/email-rahul-dev',
+    cookieKey:'dsgsdgsdfgsgsdf'
+};
